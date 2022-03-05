8a17e8488a2d0edeea09377f81774843056a3fda
Eitan <eitan.kats96@gmail.com>
2022-03-05 20:29:30 +0200

    prep for submition

--- mat.cpp
+++ mat.cpp
@@ -8,49 +8,10 @@
 
 namespace ariel {
 
-    void test(char **mat, int leftIdx, int rightIdx, int upIdx, int lowIdx, char color1, char color2) {
-        for (int j = leftIdx; j < rightIdx; ++j) {
-            mat[lowIdx][j] = '3';
-            mat[upIdx - 1][j] = '3';
-        }
-        for (int i = lowIdx; i < upIdx; ++i) {
-            mat[i][leftIdx] = '3';
-            mat[i][rightIdx - 1] = '3';
-
-        }
-
-
-    }
-
     string mat(int columns, int rows, char color1, char color2) {
-
-//        if (columns % 2 == 0 || rows % 2 == 0) {
-//            throw std::invalid_argument("Mat size is always odd");
-//        }
 
         string matAsStr;
 
-//        char **mati = new char *[rows];
-//        for (int r = 0; r < rows; ++r) {
-//            mati[r] = new char[columns];
-//        }
-//
-//        for (int r = 0; r < rows; ++r) {
-//            for (int col = 0; col < columns; ++col) {
-//                mati[r][col] = color1;
-//            }
-//        }
-
-//        test(mati, 0, columns, rows, 0, color1, color2);
-//
-//        for (int r = 0; r < rows; ++r) {
-//            for (int col = 0; col < columns; ++col) {
-//                matAsStr += mati[r][col];
-//            }
-//            matAsStr += "\n";
-//        }
-
-//        std::cout << matAsStr << std::endl;
         return matAsStr;
     }
 
