299bc6282ade3f635cc041bc39863935e52e702a
EitanKats <eitan.kats96@gmail.com>
2022-03-13 22:11:02 +0200

    added a shop loop

--- main.cpp
+++ main.cpp
@@ -5,15 +5,52 @@
 using namespace std;
 using namespace ariel;
 
+const int exitChar = -1;
+const int rugCreation = 1;
+
+
+void handleRugInput() {
+    int rows, columns;
+    char color1, color2;
+    cout << "enter rug size" << endl;
+    cin >> rows >> columns;
+    cout << "enter rug colors" << endl;
+    cin >> color1 >> color2;
+    cout << mat(columns, rows, color1, color2) << endl;
+    return;
+}
+
+void startShopLoop() {
+    cout << "press 1 to customize or -1 to confirm your order" << endl;
+    int action;
+    while (cin) {
+
+        cin >> action;
+        if (action == exitChar) {
+            return;
+        }
+        if (action == rugCreation) {
+            cout << "enter rug colors";
+            handleRugInput();
+            cout << "press 1 to customize or -1 to confirm your order" << endl;
+        }
+    }
+}
+
 int main(int argc, char **argv) {
 
     int columns, rows;
     char color1, color2;
-    if (argc < 5) {
-        cout << "You have entered " << argc - 1 << " arguments:" << endl;
-        cout << "Please provide 4 valid arguments" << endl;
-    }
+
     try {
+        if (argc == 1) {
+            startShopLoop();
+            return 0;
+        }
+        if (argc < 5) {
+            cout << "You have entered " << argc - 1 << " arguments:" << endl;
+            cout << "Please provide 4 valid arguments" << endl;
+        }
         columns = atoi(argv[1]);
         rows = atoi(argv[2]);
         if (strlen(argv[3]) > 1 || strlen(argv[4]) > 1) {
